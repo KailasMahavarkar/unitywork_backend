d00dd3a92c2c434af29cf3e88089b6fd634a594c
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2022-12-15 18:23:49 +0530

    fixed gig delete logic

--- controllers/private/sellers/deleteGig.js
+++ controllers/private/sellers/deleteGig.js
@@ -1,5 +1,6 @@
 // convert import to commonjs
 const GigModel = require("../../../models/gigModel");
+const UserModel = require("../../../models/userModel");
 
 
 const deleteGIG = async (req, res) => {
@@ -16,13 +17,51 @@ const deleteGIG = async (req, res) => {
     }
 
     try {
-        const deleteResult = await GigModel.findByIdAndDelete(gigId);
+        const gig = await GigModel.findOne({
+            _id: gigId
+        })
+
+        if (!gig) {
+            return res.status(400).json({
+                message: "Gig not found",
+                status: "failed"
+            })
+        }
+
+        const sellerName = gig.sellerUsername;
+
+        // update gig array in user model
+        const updateResult = await UserModel.findOneAndUpdate(
+            {
+                username: sellerName
+            },
+            {
+                $pull: {
+                    gigs: gigId
+                }
+            }
+        )
+
+        if (!updateResult) {
+            return res.status(400).json({
+                message: "Seller not found",
+                status: "failed"
+            })
+        }
+
+        // delete gig
+        const deleteResult = await GigModel.deleteOne({
+            _id: gigId
+        })
+
+
         if (deleteResult) {
             return res.status(200).json({
                 message: "Gig deleted successfully",
                 status: "success"
             })
         }
+
 
         return res.status(400).json({
             message: "Gig not found",
